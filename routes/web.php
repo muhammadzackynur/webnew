d72633703d8c46ebcb309f970d6ab093193250ef
Muhammad Zacky Nur <zackyclagent61@gmail.com>
2025-09-22 14:43:20 +0700

    ssdsd

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+// routes/web.php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProjectController;
 
@@ -12,6 +14,13 @@ Route::get('/project/{rowIndex}', [ProjectController::class, 'show'])->name('pro
 // Rute BARU untuk menampilkan semua foto
 Route::get('/project/{rowIndex}/gallery', [ProjectController::class, 'showAllGallery'])->name('project.gallery');
 
+// Rute untuk menambah material secara manual
 Route::post('/project/{rowIndex}/add-material', [ProjectController::class, 'addMaterial'])->name('project.addMaterial');
 
+// =================== TAMBAHKAN RUTE INI ===================
+// Rute untuk upload material dari template Excel
+Route::post('/project/{rowIndex}/upload-material-excel', [ProjectController::class, 'uploadMaterialExcel'])->name('project.uploadMaterialExcel');
+// ==========================================================
+
+// Rute untuk export material ke Excel
 Route::get('/project/{rowIndex}/export-material', [ProjectController::class, 'exportMaterial'])->name('project.exportMaterial');
